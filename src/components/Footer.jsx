1cb80f7109ab120ed69503a8044a583e9a3f4868
atul21mangla <atulmangla210503@gmail.com>
2025-08-07 18:25:33 +0530

    urls updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => {
   const socialLinks = [
     {
       title: "GitHub",
-      href: "https://github.com/DEVENWAGH",
+      href: "https://github.com/atul21mangla",
       icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -53,26 +53,8 @@ const Footer = () => {
       ),
     },
     {
-      title: "X",
-      href: "https://x.com/ntMUA4ZjcI66141",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="w-full h-full"
-        >
-          <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
-        </svg>
-      ),
-    },
-    {
       title: "LinkedIn",
-      href: "https://www.linkedin.com/in/deven-wagh-5691b7271/",
+      href: "https://www.linkedin.com/in/atul-mangla210503",
       icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -92,7 +74,7 @@ const Footer = () => {
     },
     {
       title: "Instagram",
-      href: "https://www.instagram.com/wagh_deven/",
+      href: "https://www.instagram.com/_atulmangla/",
       icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
